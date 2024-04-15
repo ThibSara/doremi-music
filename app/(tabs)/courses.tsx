a037e4da1f44462bf4348d395a2d0c1ea63c3fc3
ThibSara <sara.thibierge@gmail.com>
2024-04-15 20:40:18 +0200

    Add styles and components to Course screen

--- app/(tabs)/courses.tsx
+++ app/(tabs)/courses.tsx
@@ -1,13 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet,Dimensions } from 'react-native'
 import React from 'react'
-import {Tabs} from 'expo-router'
+import Colors from '@/constants/Colors'
+import Summary from '@/components/Summary'
+
 
 const Course = () => {
   return (
  <View>
-    <Text>Course</Text>
+    <View style={styles.container}>
+
+      <Summary/>
+      </View>
  </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    paddingTop: Dimensions.get('window').height * 0.05,
+  },
+  heading: {
+    paddingBottom: Dimensions.get('window').height * 0.02,
+    borderBottomColor: Colors.C200,
+    borderBottomWidth: 1,
+  },
+})
 
 export default Course
